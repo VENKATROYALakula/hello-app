55c8610f39af3250303b77c08093a1e50d3a346c
VENKATROYALakula <venkatakula2007@gmail.com>
2026-04-15 12:17:17 +0530

    uc3 implement

--- src/main/java/com/example/helloapp/App.java
+++ src/main/java/com/example/helloapp/App.java
@@ -4,7 +4,12 @@ package com.example.helloapp;
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+	    if (args.length > 0) {
+	        String name = args[0];
+	        System.out.println("Hello, " + name + "!");
+	    } else {
+	        System.out.println("Hello, world!");
+	    }
+	}
 }
